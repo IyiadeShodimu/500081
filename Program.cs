84ec99d53f588d159d854ca6f6b062d81ee0298a
IyiadeShodimu <talk2iyiade@gmail.com>
2022-02-08 15:14:22 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Demonstrate Sockets
+using System;
+using System.Net.Sockets;
+using System.IO;
+public class Whois
+{
+    static void Main(string[] args)
+    {
+        TcpClient client = new TcpClient();
+        client.Connect("whois.networksolutions.com", 43);
+        StreamWriter sw = new StreamWriter(client.GetStream());
+        StreamReader sr = new StreamReader(client.GetStream());
+        try 
+        {
+            sw.WriteLine(args[0]);
+            sw.Flush();
+            Console.WriteLine(sr.ReadToEnd());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("No Arguments supplied" + e);
+        }
+        
+    }
+}
